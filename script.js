5415aa00d02629d2d771e40c19228070c5b6392e
James Hawker <526rserbinenko@frhsd.com>
2025-05-09 17:48:31 +0000

    pathing?

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-
+class Cell {
+  x;
+  y;
+  color;
+  contains;
+  constructor(x, y, color, contains) {
+    (this.x = x),
+      (this.y = y),
+      (this.color = color),
+      (this.contains = contains);
+  }
+  path(...directions) {
+    let hold = this;
+    directions.forEach((e) => {
+      hold = grid[(hold.x + e.x, hold.y + e.y)];
+      if (hold.contains) {
+        
+      }
+    });
+    return
+  }
+}
